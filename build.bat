143a5bcfdff92c0d004aab791f0f316b6713a264
Krzem <krzem5.dev@gmail.com>
2021-08-21 19:48:27 +0200

    Push Update 21/08/2021, 19:48:27

--- build.bat
+++ build.bat
@@ -1,15 +1,3 @@
 @echo off
 cls
-goto build
-if not exist build\builder\main.exe (
-:build
-	if not exist build mkdir build
-	if not exist build\builder (
-		mkdir build\builder
-	) else del /s /q /f build\builder\*>nul
-	cd build\builder
-	cl.exe ../../src/builder/*.c /nologo
-	cls
-	cd ../../
-)
-build\builder\main.exe %*
+python src/_build/main.py %*
