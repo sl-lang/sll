d19efcc8d8c48a1c3dce9e4036e7f3e165ce5afc
Krzem <krzem5.dev@gmail.com>
2021-12-23 09:42:27 +0100

    Push Update 23/12/2021, 09:42:27

--- src/web/client/shell_install.sh
+++ src/web/client/shell_install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+
 wget -qO - "https://sll.krzem.workers.dev/apt/key.gpg" | sudo apt-key add -
 echo "deb https://sll.krzem.workers.dev/ apt/unstable/" | sudo tee /etc/apt/sources.list.d/sll.list
-sudo apt-get update
-sudo apt-get install sll
+sudo apt update
+sudo apt install sll
