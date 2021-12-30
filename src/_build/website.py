a2c2380f64cd888a056cd6932346f8a0a629249b
Krzem <krzem5.dev@gmail.com>
2021-12-30 21:36:42 +0100

    Push Update 30/12/2021, 21:36:42

--- src/_build/website.py
+++ src/_build/website.py
@@ -54,7 +54,7 @@ def generate():
 	toc=_generate_toc(d_dt)
 	util.log("Reading 'src/web/client/index.html'...")
 	with open("src/web/client/index.html","rb") as rf:
-		cf_a_dt=os.getenv("CLOUDFLARE_ANALYTICS",b"")
+		cf_a_dt=os.getenv("ANALYTICS",b"")
 		if (len(cf_a_dt)>0):
 			cf_a_dt=b"-->"+cf_a_dt+b"<!--"
 		o+=_add_data("/index.html",rf.read().replace(b"{{DATA}}",toc).replace(b"{{ANALYTICS}}",cf_a_dt))
