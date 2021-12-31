9ccd4de820eaf8131341890d446ee8b07cc2fa98
Krzem <krzem5.dev@gmail.com>
2021-12-31 09:42:15 +0100

    Push Update 31/12/2021, 09:42:15

--- src/web/server/main.js
+++ src/web/server/main.js
@@ -15,16 +15,33 @@ const REDIRECTS={
 
 
 
+const _get_page=async (url)=>{
+	const bf=new Uint8Array(await crypto.subtle.digest("SHA-256",new TextEncoder().encode(url)));
+	let id="";
+	for (let k of bf){
+		id+=k.toString(16).padStart(2,"0");
+	}
+	return await SLL.get(id,{
+		type: "stream"
+	});
+}
+
+
+
 addEventListener("fetch",(e)=>e.respondWith((async (url)=>{
 	url=REDIRECTS[url]||url;
-	let h={};
-	if (!(await SLL.get("__table")).split("\x00").includes(url)){
-		h.status=404;
-		h.statusText="Not Found";
-		url="not_found.html"
-	}
-	h.headers=new Headers({
-		"Content-Type":MIME_TYPES[url.split(".").at(-1)]||"text/plain;charset=utf-8"
+	const ext=url.split(".").at(-1);
+	const headers=new Headers({
+		"Content-Type": MIME_TYPES[ext]||"text/plain;charset=utf-8"
 	});
-	return new Response(await SLL.get(Array.from(new Uint8Array(await crypto.subtle.digest("SHA-256",new TextEncoder().encode(url)))).map(k=>k.toString(16).padStart(2,"0")).join(""),"arrayBuffer"),h);
+	if (!(await SLL.get("__table")).split("\x00").includes(url)){
+		return new Response((ext==="html"?await _get_page("not_found.html"):"Not Found"),{
+			status: 404,
+			statusText: "Not Found",
+			headers: headers
+		});
+	}
+	return new Response(await _get_page(url),{
+		headers: headers
+	});
 })("/"+e.request.url.match(/^https?:\/\/[a-z0-9._]+\/+([^?#]*)/)[1])));
