9adc046f371dd55b4d1cc9e6c676da0966fad02a
Krzem <krzem5.dev@gmail.com>
2022-01-28 18:58:56 +0100

    Push Update 28/01/2022, 18:58:56

--- src/sll/metadata.c
+++ src/sll/metadata.c
@@ -13,9 +13,9 @@ static sll_node_t* _mark(sll_node_t* o,bitmap_t* m){
 	}
 	switch (o->t){
 		case SLL_NODE_TYPE_CHAR:
-		case SLL_NODE_TYPE_IDENTIFIER:
 		case SLL_NODE_TYPE_INT:
 		case SLL_NODE_TYPE_FLOAT:
+		case SLL_NODE_TYPE_IDENTIFIER:
 		case SLL_NODE_TYPE_FUNCTION_ID:
 			return o+1;
 		case SLL_NODE_TYPE_STRING:
@@ -80,9 +80,9 @@ static sll_node_t* _update(sll_node_t* o,sll_string_index_t* sm){
 	}
 	switch (o->t){
 		case SLL_NODE_TYPE_CHAR:
-		case SLL_NODE_TYPE_IDENTIFIER:
 		case SLL_NODE_TYPE_INT:
 		case SLL_NODE_TYPE_FLOAT:
+		case SLL_NODE_TYPE_IDENTIFIER:
 		case SLL_NODE_TYPE_FUNCTION_ID:
 			return o+1;
 		case SLL_NODE_TYPE_STRING:
