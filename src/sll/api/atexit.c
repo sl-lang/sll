046bd0a5af6d90333bfbb59980734e6b2b19f077
Krzem <krzem5.dev@gmail.com>
2022-03-27 20:48:46 +0200

    Push Update 27/03/2022, 20:48:46

--- src/sll/api/atexit.c
+++ src/sll/api/atexit.c
@@ -4,7 +4,9 @@
 #include <sll/api.h>
 #include <sll/common.h>
 #include <sll/gc.h>
+#include <sll/init.h>
 #include <sll/memory.h>
+#include <sll/platform/lock.h>
 #include <sll/types.h>
 #include <sll/vm.h>
 
@@ -12,6 +14,13 @@
 
 static atexit_function_t** _atexit_data=NULL;
 static sll_array_length_t _atexit_data_len=0;
+static sll_lock_handle_t _atexit_lock=NULL;
+
+
+
+static void _cleanup_data(void){
+	SLL_CRITICAL(sll_platform_lock_delete(_atexit_lock));
+}
 
 
 
@@ -26,6 +35,7 @@ static void _delete_atexit_function(atexit_function_t* af){
 
 
 void _atexit_execute(void){
+	SLL_CRITICAL(sll_platform_lock_acquire(_atexit_lock));
 	while (_atexit_data_len){
 		_atexit_data_len--;
 		atexit_function_t* af=*(_atexit_data+_atexit_data_len);
@@ -34,6 +44,7 @@ void _atexit_execute(void){
 	}
 	sll_deallocate(_atexit_data);
 	_atexit_data=NULL;
+	SLL_CRITICAL(sll_platform_lock_release(_atexit_lock));
 }
 
 
@@ -42,6 +53,11 @@ __API_FUNC(atexit_register){
 	if (!a){
 		return;
 	}
+	if (!_atexit_lock){
+		_atexit_lock=sll_platform_lock_create();
+		sll_register_cleanup(_cleanup_data);
+	}
+	SLL_CRITICAL(sll_platform_lock_acquire(_atexit_lock));
 	_atexit_data_len++;
 	_atexit_data=sll_reallocate(_atexit_data,_atexit_data_len*sizeof(atexit_function_t*));
 	atexit_function_t* af=sll_allocate(sizeof(atexit_function_t)+bc*sizeof(sll_object_t*));
@@ -52,14 +68,16 @@ __API_FUNC(atexit_register){
 		af->al[i]=*(b+i);
 	}
 	*(_atexit_data+_atexit_data_len-1)=af;
+	SLL_CRITICAL(sll_platform_lock_release(_atexit_lock));
 }
 
 
 
 __API_FUNC(atexit_unregister){
-	if (!a){
+	if (!a||!_atexit_lock){
 		return 0;
 	}
+	SLL_CRITICAL(sll_platform_lock_acquire(_atexit_lock));
 	sll_bool_t o=0;
 	sll_array_length_t i=0;
 	for (sll_array_length_t j=0;j<_atexit_data_len;j++){
@@ -75,5 +93,6 @@ __API_FUNC(atexit_unregister){
 	}
 	_atexit_data_len=i;
 	_atexit_data=sll_reallocate(_atexit_data,_atexit_data_len*sizeof(atexit_function_t*));
+	SLL_CRITICAL(sll_platform_lock_release(_atexit_lock));
 	return o;
 }
