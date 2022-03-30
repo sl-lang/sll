9e31bd3cbaa9c766d14ab7c4bcbf9137059ea5cf
Krzem <krzem5.dev@gmail.com>
2022-03-30 18:47:33 +0200

    Push Update 30/03/2022, 18:47:33

--- build.bat
+++ build.bat
@@ -1,3 +1,9 @@
 @echo off
 if not exist build mkdir build
+if not exist build\_sll_runtime_windows (
+	mkdir build\_sll_runtime_windows
+	curl -o build\_sll_runtime_windows\windows.zip https://sll.krzem.workers.dev/bin/windows.zip
+	powershell -command "Expand-Archive -Force 'build\_sll_runtime_windows\windows.zip' 'build\_sll_runtime_windows\'"
+	del /f /q build\_sll_runtime_windows\windows.zip
+)
 python src\_build\main.py %*
--- build.sh
+++ build.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 [ ! -d "build" ]&&mkdir build
+if [ "$(uname -s)" == "Darwin" ]; then
+	platform="darwin"
+else
+	platform="linux"
+fi
+if [ ! -d "build/_sll_runtime_$platform" ]; then
+	mkdir "build/_sll_runtime_$platform"
+	curl -o "build/_sll_runtime_$platform/$platform.zip" "https://sll.krzem.workers.dev/bin/$platform.zip"
+	unzip -qq -o "build/_sll_runtime_$platform/$platform.zip" -d "build/_sll_runtime_$platform/"
+	rm -f "build/_sll_runtime_$platform/$platform.zip"
+fi
 python3 src/_build/main.py "$@"
