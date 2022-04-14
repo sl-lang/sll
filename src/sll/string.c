6f0ca3e602ebf9f13148f9e323552dfb3f8719e1
Krzem <krzem5.dev@gmail.com>
2022-04-14 18:34:35 +0200

    Push Update 14/04/2022, 18:34:35

--- src/sll/string.c
+++ src/sll/string.c
@@ -957,8 +957,8 @@ __SLL_EXTERNAL __SLL_CHECK_OUTPUT sll_string_length_t sll_string_index_multiple(
 		}
 		v&=0x8080808080808080ull;
 		if (v){
-			sll_string_length_t o=(i<<3)+(FIND_FIRST_SET_BIT(v)>>3);
 			sll_deallocate(ml);
+			sll_string_length_t o=(i<<3)+(FIND_FIRST_SET_BIT(v)>>3);
 			return (o>=s->l?SLL_MAX_STRING_LENGTH:o);
 		}
 		p++;
@@ -993,8 +993,8 @@ __SLL_EXTERNAL __SLL_CHECK_OUTPUT sll_string_length_t sll_string_index_multiple(
 		}
 		v&=0x8080808080808080ull;
 		if (v){
-			sll_string_length_t o=(i<<3)+(FIND_FIRST_SET_BIT(v)>>3);
 			sll_deallocate(ml);
+			sll_string_length_t o=(i<<3)+(FIND_FIRST_SET_BIT(v)>>3);
 			return (o>=s->l?SLL_MAX_STRING_LENGTH:o);
 		}
 		p++;
@@ -1059,6 +1059,7 @@ __SLL_EXTERNAL __SLL_CHECK_OUTPUT sll_string_length_t sll_string_index_reverse_m
 	sll_string_length_t l=(s->l+7)>>3;
 	const wide_data_t* p=((const wide_data_t*)(s->v))+l;
 	ASSUME_ALIGNED(p,3,0);
+#ifdef __SLL_BUILD_DARWIN
 	wide_data_t* ml=sll_allocate_stack(cll*sizeof(wide_data_t));
 	for (sll_string_length_t i=0;i<cll;i++){
 		*(ml+i)=0x101010101010101ull*(*(cl+i));
@@ -1082,12 +1083,55 @@ __SLL_EXTERNAL __SLL_CHECK_OUTPUT sll_string_length_t sll_string_index_reverse_m
 					continue;
 				}
 			}
-			sll_string_length_t o=((l-i-1)<<3)+(FIND_LAST_SET_BIT(v)>>3);
 			sll_deallocate(ml);
+			sll_string_length_t o=((l-i-1)<<3)+(FIND_LAST_SET_BIT(v)>>3);
 			return (o>=s->l?SLL_MAX_STRING_LENGTH:o);
 		}
 	}
 	sll_deallocate(ml);
+#else
+	sll_string_length_t ln=(cll+3)&0xfffffffc;
+	wide_data_t* ml=sll_allocate_stack(ln*sizeof(wide_data_t));
+	sll_string_length_t i=0;
+	for (;i<cll-1;i++){
+		*(ml+i)=0x101010101010101ull*(*(cl+i));
+	}
+	wide_data_t any=0x101010101010101ull*(*(cl+i));
+	for (;i<ln;i++){
+		*(ml+i)=any;
+	}
+	ln>>=2;
+	__m256i sub=_mm256_set1_epi8(1);
+	for (sll_string_length_t i=0;i<l;i++){
+		p--;
+		__m256i k=_mm256_set1_epi64x(*p);
+		__m256i v256=_mm256_setzero_si256();
+		const __m256i* m=(const __m256i*)ml;
+		for (sll_string_length_t j=0;j<ln;j++){
+			__m256i e=_mm256_xor_si256(k,_mm256_lddqu_si256(m));
+			m++;
+			v256=_mm256_or_si256(v256,_mm256_andnot_si256(e,_mm256_sub_epi64(e,sub)));
+		}
+		v256=_mm256_or_si256(v256,_mm256_permute2f128_si256(v256,v256,1));
+		wide_data_t v=_mm256_extract_epi64(_mm256_or_si256(v256,_mm256_shuffle_epi32(v256,0x4e)),0);
+		if (inv){
+			v=~v;
+		}
+		v&=0x8080808080808080ull;
+		if (v){
+			if (!i&&(s->l&7)){
+				v&=0xffffffffffffffffull>>((8-s->l)<<3);
+				if (!v){
+					continue;
+				}
+			}
+			sll_deallocate(ml);
+			sll_string_length_t o=((l-i-1)<<3)+(FIND_LAST_SET_BIT(v)>>3);
+			return (o>=s->l?SLL_MAX_STRING_LENGTH:o);
+		}
+	}
+	sll_deallocate(ml);
+#endif
 	return SLL_MAX_STRING_LENGTH;
 }
 
