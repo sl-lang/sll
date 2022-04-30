05dccf00d5d39fe9f678e371e9ab0c3e9e116acc
Krzem <krzem5.dev@gmail.com>
2022-04-30 21:41:22 +0200

    Push Update 30/04/2022, 21:41:22

--- src/sll/allocator.c
+++ src/sll/allocator.c
@@ -15,31 +15,31 @@ __SLL_EXTERNAL void sll_allocator_collapse(void** ptr,sll_size_t size){
 	}
 	SLL_ASSERT(!(size&7));
 	size>>=3;
-	allocator_header_t* h=PTR(ADDR(*ptr)-sizeof(allocator_header_t));
-	if (ALLOCATOR_HEADER_GET_SIZE(h)==size){
+	allocator_header_t* header=PTR(ADDR(*ptr)-sizeof(allocator_header_t));
+	if (ALLOCATOR_HEADER_GET_SIZE(header)==size){
 		return;
 	}
-	ALLOCATOR_HEADER_INIT(h,size);
-	h=sll_reallocate(h,(size<<3)+sizeof(allocator_header_t));
-	*ptr=PTR(ADDR(h)+sizeof(allocator_header_t));
+	ALLOCATOR_HEADER_INIT(header,size);
+	header=sll_reallocate(header,(size<<3)+sizeof(allocator_header_t));
+	*ptr=PTR(ADDR(header)+sizeof(allocator_header_t));
 }
 
 
 
 __SLL_EXTERNAL __SLL_CHECK_OUTPUT void* sll_allocator_from_memory(void* ptr,sll_size_t size){
-	void* o=sll_allocator_init(size);
-	sll_copy_data(ptr,size,o);
-	return o;
+	void* out=sll_allocator_init(size);
+	sll_copy_data(ptr,size,out);
+	return out;
 }
 
 
 
 __SLL_EXTERNAL __SLL_CHECK_OUTPUT void* sll_allocator_init(sll_size_t size){
 	SLL_ASSERT(!(size&7));
-	allocator_header_t* o=sll_allocate(size+sizeof(allocator_header_t));
-	SLL_ASSERT(!(ADDR(o)&7));
-	ALLOCATOR_HEADER_INIT(o,size>>3);
-	return PTR(ADDR(o)+sizeof(allocator_header_t));
+	allocator_header_t* out=sll_allocate(size+sizeof(allocator_header_t));
+	SLL_ASSERT(!(ADDR(out)&7));
+	ALLOCATOR_HEADER_INIT(out,size>>3);
+	return PTR(ADDR(out)+sizeof(allocator_header_t));
 }
 
 
@@ -69,12 +69,12 @@ __SLL_EXTERNAL void sll_allocator_resize(void** ptr,sll_size_t size){
 	}
 	SLL_ASSERT(!(size&7));
 	size>>=3;
-	allocator_header_t* h=PTR(ADDR(*ptr)-sizeof(allocator_header_t));
-	if (ALLOCATOR_HEADER_GET_SIZE(h)>=size&&size>=(ALLOCATOR_HEADER_GET_SIZE(h)>>1)){
+	allocator_header_t* header=PTR(ADDR(*ptr)-sizeof(allocator_header_t));
+	if (ALLOCATOR_HEADER_GET_SIZE(header)>=size&&size>=(ALLOCATOR_HEADER_GET_SIZE(header)>>1)){
 		return;
 	}
 	sll_size_t mem_size=(!size?0:size+(size>>3)+(size>>6));
-	ALLOCATOR_HEADER_INIT(h,mem_size);
-	h=sll_reallocate(h,(mem_size<<3)+sizeof(allocator_header_t));
-	*ptr=PTR(ADDR(h)+sizeof(allocator_header_t));
+	ALLOCATOR_HEADER_INIT(header,mem_size);
+	header=sll_reallocate(header,(mem_size<<3)+sizeof(allocator_header_t));
+	*ptr=PTR(ADDR(header)+sizeof(allocator_header_t));
 }
