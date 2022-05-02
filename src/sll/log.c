c1daa3f528fd4b18188ab246e459a8b435bced96
Krzem <krzem5.dev@gmail.com>
2022-05-02 16:20:58 +0200

    Push Update 02/05/2022, 16:20:58

--- src/sll/log.c
+++ src/sll/log.c
@@ -20,8 +20,8 @@
 
 
 static sll_logger_flags_t _log_default=0;
-static file_log_data_t** _log_f_dt=NULL;
-static sll_array_length_t _log_f_dtl=0;
+static file_log_data_t** _log_file_data=NULL;
+static sll_array_length_t _log_file_data_len=0;
 
 
 
@@ -33,21 +33,21 @@ static file_log_data_t* _get_file_index(const sll_char_t* fp){
 	sll_string_from_pointer_length(fp+i,fp_s.l-i,&s);
 	sll_free_string(&fp_s);
 	sll_array_length_t j=0;
-	for (;j<_log_f_dtl;j++){
-		file_log_data_t* k=*(_log_f_dt+j);
+	for (;j<_log_file_data_len;j++){
+		file_log_data_t* k=*(_log_file_data+j);
 		if (STRING_EQUAL(&s,&(k->nm))){
 			sll_free_string(&s);
 			return k;
 		}
 	}
-	_log_f_dtl++;
-	_log_f_dt=sll_reallocate(_log_f_dt,_log_f_dtl*sizeof(file_log_data_t*));
+	_log_file_data_len++;
+	_log_file_data=sll_reallocate(_log_file_data,_log_file_data_len*sizeof(file_log_data_t*));
 	file_log_data_t* n=sll_allocate(sizeof(file_log_data_t));
 	sll_copy_data(&s,sizeof(sll_string_t),(sll_string_t*)(&(n->nm)));
 	n->dt=NULL;
 	n->dtl=0;
 	n->fl=_log_default;
-	*(_log_f_dt+j)=n;
+	*(_log_file_data+j)=n;
 	return n;
 }
 
@@ -99,9 +99,9 @@ static void _log_location(const sll_string_t* fp,const sll_string_t* fn,sll_file
 
 
 void _log_release_data(void){
-	while (_log_f_dtl){
-		_log_f_dtl--;
-		file_log_data_t* k=*(_log_f_dt+_log_f_dtl);
+	while (_log_file_data_len){
+		_log_file_data_len--;
+		file_log_data_t* k=*(_log_file_data+_log_file_data_len);
 		sll_free_string((sll_string_t*)(&(k->nm)));
 		for (sll_arg_count_t i=0;i<k->dtl;i++){
 			function_log_data_t* e=*(k->dt+i);
@@ -111,8 +111,8 @@ void _log_release_data(void){
 		sll_deallocate(k->dt);
 		sll_deallocate(k);
 	}
-	sll_deallocate(_log_f_dt);
-	_log_f_dt=NULL;
+	sll_deallocate(_log_file_data);
+	_log_file_data=NULL;
 }
 
 
@@ -142,7 +142,7 @@ __SLL_EXTERNAL sll_bool_t sll_log(const sll_char_t* fp,const sll_char_t* fn,sll_
 
 
 __SLL_EXTERNAL sll_bool_t sll_log_raw(const sll_char_t* fp,const sll_char_t* fn,sll_file_offset_t ln,sll_bool_t w,const sll_string_t* s){
-	if (!_log_default&&!_log_f_dtl){
+	if (!_log_default&&!_log_file_data_len){
 		return 0;
 	}
 	file_log_data_t* f_dt=_get_file_index(fp);
