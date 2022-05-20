edaa00857f7502e226772c69eb3333a1fddc83bf
Krzem <krzem5.dev@gmail.com>
2022-05-20 21:10:19 +0200

    Push Update 20/05/2022, 21:10:19

--- src/_build/bundle.py
+++ src/_build/bundle.py
@@ -4,15 +4,6 @@ import zipfile
 
 
 
-EXECUTABLE_EXTENSION={"darwin":"","linux":"","windows":".exe"}
-LIBRARY_EXTENSION={"darwin":".so","linux":".so","windows":".dll"}
-
-
-
-system=platform.system().lower()
-with zipfile.ZipFile(system+".zip","w",compression=zipfile.ZIP_DEFLATED) as zf:
-	for k in ["build/sll"+EXECUTABLE_EXTENSION[system],"build/sll-"+sys.argv[1]+LIBRARY_EXTENSION[system],"build/lib/stdlib.slb","build/lib_debug/stdlib.slb"]:
+with zipfile.ZipFile(platform.system().lower()+".zip","w",compression=zipfile.ZIP_DEFLATED) as zf:
+	for k in sys.argv[1:]:
 		zf.write(k,arcname=k[6:])
-	if (system=="windows"):
-		zf.write("build/sllw.exe",arcname="sllw.exe")
-	zf.write("build/sll.h",arcname="include/sll.h")
