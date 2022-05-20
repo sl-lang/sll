c57e9a5ef97d0f3774bceb5b216dc810ffd9182a
Krzem <krzem5.dev@gmail.com>
2022-05-20 20:56:26 +0200

    Push Update 20/05/2022, 20:56:26

--- src/_build/main.py
+++ src/_build/main.py
@@ -5,8 +5,3 @@ import util
 
 if (util.execute([util.sll_runtime,"src/_build/main.sll","-I","src/sll/lib","-I","src/_build","-A"]+sys.argv)):
 	sys.exit(0)
-if ("--web" in sys.argv):
-	sys.exit(0)
-if ("--bundle" in sys.argv):
-	util.log("Compressing executable files...")
-	util.bundle()
--- src/_build/util.py
+++ src/_build/util.py
@@ -11,29 +11,10 @@ LIBRARY_EXTENSION={"darwin":".so","linux":".so","windows":".dll"}
 
 
 
-def _read_version(fp):
-	o=[None,None,None]
-	with open(fp,"r") as f:
-		for k in f.read().replace("\r\n","\n").split("\n"):
-			k=k.strip()
-			if (k[:1]=="#"):
-				k=k[1:].lstrip().split(" ")
-				if (k[0]=="define"):
-					if (k[1]=="SLL_VERSION_MAJOR"):
-						o[0]=k[2]
-					elif (k[1]=="SLL_VERSION_MINOR"):
-						o[1]=k[2]
-					elif (k[1]=="SLL_VERSION_PATCH"):
-						o[2]=k[2]
-	return ".".join(o)
-
-
-
 log=(print if "--verbose" in sys.argv else lambda _:None)
 system=platform.system().lower()
 sll_runtime=f"build/_sll_runtime_{system}/sll"+EXECUTABLE_EXTENSION[system]
 verbose=("--verbose" in sys.argv)
-version=_read_version("src/sll/include/sll/version.h")
 
 
 
@@ -43,9 +24,9 @@ def execute(args):
 
 
 
-def bundle():
+if (__name__=="__main__"):
 	with zipfile.ZipFile(system+".zip","w",compression=zipfile.ZIP_DEFLATED) as zf:
-		for k in ["build/sll"+EXECUTABLE_EXTENSION[system],"build/sll-"+version+LIBRARY_EXTENSION[system],"build/lib/stdlib.slb","build/lib_debug/stdlib.slb"]:
+		for k in ["build/sll"+EXECUTABLE_EXTENSION[system],"build/sll-"+sys.argv[1]+LIBRARY_EXTENSION[system],"build/lib/stdlib.slb","build/lib_debug/stdlib.slb"]:
 			zf.write(k,arcname=k[6:])
 		if (system=="windows"):
 			zf.write("build/sllw.exe",arcname="sllw.exe")
