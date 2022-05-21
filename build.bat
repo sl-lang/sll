9b85b584f11c5c339b66f0da68fce4bc6b6587da
Krzem <krzem5.dev@gmail.com>
2022-05-21 08:53:32 +0200

    Push Update 21/05/2022, 08:53:32

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@
 if not exist build mkdir build
 if not exist build\_sll_runtime_windows (
 	mkdir build\_sll_runtime_windows
-	curl -o build\_sll_runtime_windows\windows.zip https://sll.krzem.workers.dev/bin/nightly/windows.zip
+	curl -o build\_sll_runtime_windows\windows.zip https://sll.krzem.workers.dev/bin/windows.zip
 	powershell -command "Expand-Archive -Force 'build\_sll_runtime_windows\windows.zip' 'build\_sll_runtime_windows\'"
 	del /f /q build\_sll_runtime_windows\windows.zip
 )
--- changelog/latest.md
+++ changelog/latest.md
@@ -1,5 +1,7 @@
 # Sll [0.7.19] - Ongoing
 
-*Nothing*
+## Changed
+
+- Build script no longer uses nightly builds
 
 [0.7.19]: https://github.com/sl-lang/sll/compare/sll-v0.7.18...main
