596ba0700bf40173bb6c246fb375a2d0ccb711dc
Krzem <krzem5.dev@gmail.com>
2022-05-21 16:21:44 +0200

    Push Update 21/05/2022, 16:21:44

--- src/_build/bundle.py
+++
@@ -1,9 +0,0 @@
-import platform
-import sys
-import zipfile
-
-
-
-with zipfile.ZipFile(platform.system().lower()+".zip","w",compression=zipfile.ZIP_DEFLATED) as zf:
-	for k in sys.argv[1:]:
-		zf.write(k,arcname=k[6:])
