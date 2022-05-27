24f29f49d273c71feb37684fad6bfe8e1e895c43
Krzem <krzem5.dev@gmail.com>
2022-05-27 22:37:12 +0200

    Push Update 27/05/2022, 22:37:12

--- src/web/apt.sh
+++ src/web/apt.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl -s "https://sll.krzem.workers.dev/apt/key.gpg" | apt-key add -
-echo "deb https://sll.krzem.workers.dev/ apt/unstable/" | tee /etc/apt/sources.list.d/sll.list
+curl -s "https://sl-lang.github.io/apt/key.gpg" | apt-key add -
+echo "deb https://sl-lang.github.io/ apt/unstable/" | tee /etc/apt/sources.list.d/sll.list
 apt update
 apt install sll
