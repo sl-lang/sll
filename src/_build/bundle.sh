4dca996890911a0372d0b11afa3ac70afd03b61f
Krzem <krzem5.dev@gmail.com>
2022-05-27 20:40:14 +0200

    Push Update 27/05/2022, 20:40:14

--- src/_build/bundle.sh
+++ src/_build/bundle.sh
@@ -4,6 +4,7 @@ if [ "$(uname -s)" == "Darwin" ]; then
 else
 	platform="linux"
 fi
-cd build
-zip -9 "../$platform.zip" sll "sll-`cat version`.so" sll.h lib/stdlib.slb lib_debug/stdlib.slb
-cd ..
+(
+	cd build||exit
+	zip -9 "../$platform.zip" sll "sll-$(cat version).so" sll.h lib/stdlib.slb lib_debug/stdlib.slb
+)
