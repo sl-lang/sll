872dbe3971c7d47b14c0c1aaf225019cf1a8e9ec
Krzem <krzem5.dev@gmail.com>
2022-05-28 09:42:12 +0200

    Push Update 28/05/2022, 09:42:12

--- src/web/apt.sh
+++ src/web/apt.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-curl -s "https://sl-lang.github.io/apt/key.gpg" | apt-key add -
-echo "deb https://sl-lang.github.io/ apt/unstable/" | tee /etc/apt/sources.list.d/sll.list
+[ ! -d "/usr/local/share/keyrings" ]&&mkdir /usr/local/share/keyrings
+curl -s "https://sl-lang.github.io/pkg/apt/key.asc" > /usr/local/share/keyrings/sll.asc
+rm -f /tmp/sll-keyring.gpg
+echo "deb [signed-by=/usr/local/share/keyrings/sll.asc] https://sl-lang.github.io/ pkg/apt/unstable/" > /etc/apt/sources.list.d/sll.list
 apt update
 apt install sll
