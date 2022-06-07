9330ec734ef6a9fbd74eedb58f7375d7b3599955
Krzem <krzem5.dev@gmail.com>
2022-06-07 08:36:33 +0200

    Push Update 07/06/2022, 08:36:33

--- src/sll/api/audit.c
+++ src/sll/api/audit.c
@@ -14,20 +14,20 @@
 
 
 
-static sll_integer_t _audit_cb=0;
+static sll_integer_t _audit_callback=0;
 
 
 
-static void _call_user_cb(const sll_string_t* nm,const sll_array_t* arg){
-	if (!_audit_cb){
+static void _call_user_callback(const sll_string_t* name,const sll_array_t* args){
+	if (!_audit_callback){
 		return;
 	}
 	sll_object_t* dt[2]={
-		STRING_TO_OBJECT(nm),
-		sll_array_to_object(arg)
+		STRING_TO_OBJECT(name),
+		sll_array_to_object(args)
 	};
 	sll_gc_add_roots(dt,2);
-	SLL_RELEASE(sll_execute_function(_audit_cb,dt,2,EXECUTE_FUNCTION_NO_AUDIT_TERMINATE));
+	SLL_RELEASE(sll_execute_function(_audit_callback,dt,2,EXECUTE_FUNCTION_NO_AUDIT_TERMINATE));
 	sll_gc_remove_roots(dt);
 	SLL_RELEASE(dt[0]);
 	SLL_RELEASE(dt[1]);
@@ -36,27 +36,27 @@ static void _call_user_cb(const sll_string_t* nm,const sll_array_t* arg){
 
 
 static void _cleanup_data(void){
-	sll_audit_unregister_callback(_call_user_cb);
-	_audit_cb=0;
+	sll_audit_unregister_callback(_call_user_callback);
+	_audit_callback=0;
 }
 
 
 
-__SLL_EXTERNAL __SLL_API_CALL void sll_api_audit__init(sll_integer_t cb){
-	if (!cb){
+__SLL_EXTERNAL __SLL_API_CALL void sll_api_audit__init(sll_integer_t callback){
+	if (!callback){
 		return;
 	}
-	if (!_audit_cb){
-		sll_audit_register_callback(_call_user_cb);
+	if (!_audit_callback){
+		sll_audit_register_callback(_call_user_callback);
 		sll_register_cleanup(_cleanup_data,SLL_CLEANUP_TYPE_VM);
 	}
-	_audit_cb=cb;
+	_audit_callback=callback;
 }
 
 
 
-__SLL_EXTERNAL __SLL_API_CALL void sll_api_audit_audit(const sll_string_t* name,const sll_string_t* fmt,sll_object_t*const* args,sll_arg_count_t len){
+__SLL_EXTERNAL __SLL_API_CALL void sll_api_audit_audit(const sll_string_t* name,const sll_string_t* format,sll_object_t*const* args,sll_arg_count_t arg_count){
 	sll_var_arg_list_t dt;
-	SLL_VAR_ARG_INIT_SLL(&dt,args,len);
-	sll_audit_list(name->data,fmt->data,&dt);
+	SLL_VAR_ARG_INIT_SLL(&dt,args,arg_count);
+	sll_audit_list(name->data,format->data,&dt);
 }
