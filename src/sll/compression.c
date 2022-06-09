56482e1d44d73c52adfe9514b4af920b7238e96b
Krzem <krzem5.dev@gmail.com>
2022-06-09 13:02:41 +0200

    Push Update 09/06/2022, 13:02:41

--- src/sll/compression.c
+++ src/sll/compression.c
@@ -12,23 +12,23 @@ __SLL_EXTERNAL __SLL_CHECK_OUTPUT sll_compressed_integer_t sll_compress_integer(
 		return int_;
 	}
 	sll_size_t off=0;
-	__SLL_U32 s=FIND_LAST_SET_BIT(int_);
-	SLL_ASSERT(s>=7);
+	__SLL_U32 shift=FIND_LAST_SET_BIT(int_);
+	SLL_ASSERT(shift>=7);
 	if (int_&(int_-1)){
-		__SLL_U32 m=s-1;
-		sll_size_t n=1ull<<s;
-		if ((3ull<<m)<int_){
-			s++;
+		__SLL_U32 new_shift=shift-1;
+		sll_size_t n=1ull<<shift;
+		if ((3ull<<new_shift)<int_){
+			shift++;
 			off=(((n<<1)-int_)<<1)-1;
 		}
 		else{
 			off=(int_-n)<<1;
 		}
-		if (off>>58||FIND_LAST_SET_BIT(off)>=m-5){
+		if (off>>58||FIND_LAST_SET_BIT(off)>=new_shift-5){
 			return int_;
 		}
 	}
-	return (s==64?0:s-6)|(off<<6);
+	return (shift==64?0:shift-6)|(off<<6);
 }
 
 
@@ -38,9 +38,9 @@ __SLL_EXTERNAL __SLL_CHECK_OUTPUT sll_size_t sll_decompress_integer(sll_compress
 	if (compressed_int&64){
 		o=~o;
 	}
-	sll_size_t s=compressed_int&63;
-	if (s){
-		o+=1ull<<(s+6);
+	sll_size_t shift=compressed_int&63;
+	if (shift){
+		o+=1ull<<(shift+6);
 	}
 	return o;
 }
