425be1bb0b04dc495bb6deddd45e7769d0935b25
Krzem <krzem5.dev@gmail.com>
2022-06-10 09:12:35 +0200

    Push Update 10/06/2022, 09:12:35

--- src/sll/include/sll/generated/optimizer.h
+++ src/sll/include/sll/generated/optimizer.h
@@ -7,6 +7,7 @@
 #include <../optimizer/optimize_loops.h>
 #include <../optimizer/optimize_generator_loops.h>
 #include <../optimizer/optimize_function.h>
+#include <../optimizer/optimize_internal_function.h>
 #include <../optimizer/optimize_comma.h>
 #include <../optimizer/optimize_operation_list.h>
 
@@ -33,6 +34,7 @@ static __SLL_FORCE_INLINE void _visit_optimizer(sll_source_file_t* source_file,s
 	node=__optimizer_optimize_loops(source_file,node,children,parent);
 	node=__optimizer_optimize_generator_loops(source_file,node,children,parent);
 	node=__optimizer_optimize_function(source_file,node,children,parent);
+	node=__optimizer_optimize_internal_function(source_file,node,children,parent);
 	node=__optimizer_optimize_comma(source_file,node,children,parent);
 	__optimizer_optimize_operation_list(source_file,node,children,parent);
 }
--- src/sll/optimizer/optimize_internal_function.h
+++ src/sll/optimizer/optimize_internal_function.h
@@ -9,6 +9,10 @@ OPTIMIZER_FUNTION(optimize_internal_function){
 		return node;
 	}
 	sll_arg_count_t ac=node->data.arg_count;
+	if (ac&&(*children)->type!=SLL_NODE_TYPE_STRING){
+		node->type=SLL_NODE_TYPE_OPERATION_LIST;
+		return node;
+	}
 	for (sll_arg_count_t i=1;i<ac;i++){
 		_unneeded_result(&(node->data.arg_count),*(children+i));
 	}
