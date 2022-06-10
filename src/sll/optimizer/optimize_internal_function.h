83f54c900510dfbf2942672927f62c60df76e785
Krzem <krzem5.dev@gmail.com>
2022-06-10 09:08:55 +0200

    Push Update 10/06/2022, 09:08:55

---
+++ src/sll/optimizer/optimize_internal_function.h
@@ -0,0 +1,16 @@
+#include <sll/_internal/optimizer.h>
+#include <sll/node.h>
+#include <sll/types.h>
+
+
+
+OPTIMIZER_FUNTION(optimize_internal_function){
+	if (node->type!=SLL_NODE_TYPE_INTERNAL_FUNC){
+		return node;
+	}
+	sll_arg_count_t ac=node->data.arg_count;
+	for (sll_arg_count_t i=1;i<ac;i++){
+		_unneeded_result(&(node->data.arg_count),*(children+i));
+	}
+	return node;
+}
