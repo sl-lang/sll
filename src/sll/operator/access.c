a994531cc22417ab34d4d172433632ac3dad0fae
Krzem <krzem5.dev@gmail.com>
2022-06-12 09:07:12 +0200

    Push Update 12/06/2022, 09:07:12

--- src/sll/operator/access.c
+++ src/sll/operator/access.c
@@ -50,61 +50,35 @@ __SLL_EXTERNAL __SLL_CHECK_OUTPUT sll_object_t* sll_operator_access(sll_object_t
 
 
 __SLL_EXTERNAL __SLL_CHECK_OUTPUT sll_object_t* sll_operator_access_range(sll_object_t* a,sll_object_t* b,sll_object_t* c){
-	if (a->type==SLL_OBJECT_TYPE_STRING){
-		if (b->type==SLL_OBJECT_TYPE_INT&&c->type==SLL_OBJECT_TYPE_INT){
+	if (b->type==SLL_OBJECT_TYPE_INT&&c->type==SLL_OBJECT_TYPE_INT){
+		if (a->type==SLL_OBJECT_TYPE_STRING){
 			sll_object_t* o=sll_create_object(SLL_OBJECT_TYPE_STRING);
 			sll_string_select(&(a->data.string),b->data.int_,c->data.int_,1,&(o->data.string));
 			return o;
 		}
-	}
-	else if (a->type==SLL_OBJECT_TYPE_ARRAY){
-		if (b->type==SLL_OBJECT_TYPE_INT&&c->type==SLL_OBJECT_TYPE_INT){
+		else if (a->type==SLL_OBJECT_TYPE_ARRAY){
 			sll_object_t* o=sll_create_object(SLL_OBJECT_TYPE_ARRAY);
 			sll_array_select(&(a->data.array),b->data.int_,c->data.int_,1,&(o->data.array));
 			return o;
 		}
 	}
-	else if (a->type==SLL_OBJECT_TYPE_MAP){
-		sll_object_t* o=sll_create_object(SLL_OBJECT_TYPE_MAP);
-		sll_object_t* l[2]={b,c};
-		sll_array_t arr={
-			2,
-			l
-		};
-		sll_map_and_array(&(a->data.map),&arr,&(o->data.map));
-		return o;
-	}
-	SLL_ACQUIRE(a);
-	return a;
+	return SLL_ACQUIRE_STATIC_INT(0);
 }
 
 
 
 __SLL_EXTERNAL __SLL_CHECK_OUTPUT sll_object_t* sll_operator_access_range_step(sll_object_t* a,sll_object_t* b,sll_object_t* c,sll_object_t* d){
-	if (a->type==SLL_OBJECT_TYPE_STRING){
-		if (b->type==SLL_OBJECT_TYPE_INT&&c->type==SLL_OBJECT_TYPE_INT&&d->type==SLL_OBJECT_TYPE_INT){
+	if (b->type==SLL_OBJECT_TYPE_INT&&c->type==SLL_OBJECT_TYPE_INT&&d->type==SLL_OBJECT_TYPE_INT){
+		if (a->type==SLL_OBJECT_TYPE_STRING){
 			sll_object_t* o=sll_create_object(SLL_OBJECT_TYPE_STRING);
 			sll_string_select(&(a->data.string),b->data.int_,c->data.int_,d->data.int_,&(o->data.string));
 			return o;
 		}
-	}
-	else if (a->type==SLL_OBJECT_TYPE_ARRAY){
-		if (b->type==SLL_OBJECT_TYPE_INT&&c->type==SLL_OBJECT_TYPE_INT&&d->type==SLL_OBJECT_TYPE_INT){
+		else if (a->type==SLL_OBJECT_TYPE_ARRAY){
 			sll_object_t* o=sll_create_object(SLL_OBJECT_TYPE_ARRAY);
 			sll_array_select(&(a->data.array),b->data.int_,c->data.int_,d->data.int_,&(o->data.array));
 			return o;
 		}
 	}
-	else if (a->type==SLL_OBJECT_TYPE_MAP){
-		sll_object_t* o=sll_create_object(SLL_OBJECT_TYPE_MAP);
-		sll_object_t* l[3]={b,c,d};
-		sll_array_t arr={
-			3,
-			l
-		};
-		sll_map_and_array(&(a->data.map),&arr,&(o->data.map));
-		return o;
-	}
-	SLL_ACQUIRE(a);
-	return a;
+	return SLL_ACQUIRE_STATIC_INT(0);
 }
