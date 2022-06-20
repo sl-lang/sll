a46890f087995df93436e7f6fb1239370d844d02
Krzem <krzem5.dev@gmail.com>
2022-06-20 16:06:47 +0200

    Push Update 20/06/2022, 16:06:47

--- src/sll/assembly.c
+++ src/sll/assembly.c
@@ -744,8 +744,6 @@ static const sll_node_t* _generate_assign(const sll_node_t* o,assembly_generator
 		l-=2;
 	}
 	else{
-		SLL_ASSERT(io->type==SLL_NODE_TYPE_ACCESS);
-		SLL_ASSERT(io->data.arg_count>=2);
 		o++;
 		while (o->type==SLL_NODE_TYPE_NOP||o->type==SLL_NODE_TYPE_DBG||o->type==SLL_NODE_TYPE_CHANGE_STACK){
 			GENERATE_DEBUG_DATA(g_dt,o);
