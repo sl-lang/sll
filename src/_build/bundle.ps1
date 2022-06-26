ca2585b78d59995af66e2bffa16b67bb3bf5090f
Krzem <krzem5.dev@gmail.com>
2022-06-26 18:38:31 +0200

    Push Update 26/06/2022, 18:38:31

--- src/_build/bundle.ps1
+++ src/_build/bundle.ps1
@@ -1,6 +1,7 @@
 Set-Location -Path "build"
+$version = (Get-Content version -Raw)
 $config = @{
-  Path = "sll.exe", "sllw.exe", ("sll-"+(Get-Content version -Raw)+".dll"), ("sll-"+(Get-Content version -Raw)+".lib"), "lib\", "lib_debug\", "sys_lib\"
+  Path = "sll.exe", "sllw.exe", "sll-$version.dll", "sll-$version.lib", "lib\", "lib_debug\", "sys_lib\"
   CompressionLevel = "Optimal"
   DestinationPath = "..\windows.zip"
 }
