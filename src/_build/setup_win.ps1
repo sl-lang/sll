0052191916bea0220f939ebb7c1d7110d29e25a2
Krzem <krzem5.dev@gmail.com>
2022-07-01 13:27:25 +0200

    Push Update 01/07/2022, 13:27:25

--- src/_build/setup_win.ps1
+++ src/_build/setup_win.ps1
@@ -1,7 +1,7 @@
 Invoke-WebRequest "https://www.nasm.us/pub/nasm/releasebuilds/2.15.05/win64/nasm-2.15.05-win64.zip" -OutFile "$ENV:TMP/nasm.zip"
 Expand-Archive -Force -LiteralPath "$ENV:TMP/nasm.zip" -DestinationPath "$ENV:TMP/nasm"
-$vswhere_output = (& "${Env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -products '*' -latest -prerelease -property installationPath) + "\VC\Auxiliary\Build\vcvarsall.bat"
-foreach ($elem in @(& "$vswhere_output" x64 "&&" set)) {
+$vcvarsall = (& "${Env:ProgramFiles(x86)}\Microsoft Visual Studio\Installer\vswhere.exe" -products '*' -latest -prerelease -property installationPath) + "\VC\Auxiliary\Build\vcvarsall.bat"
+foreach ($elem in @(& "$vcvarsall" x64 "&&" set)) {
 	$elem=$elem.Split("=")
 	if ($elem[0] -eq "PATH"){
 		$PATH=$elem[1]
