a6997171e905a02487672945481587c8c1a779d1
Krzem <krzem5.dev@gmail.com>
2022-07-02 17:06:32 +0200

    Push Update 02/07/2022, 17:06:32

--- src/ext/window/platform/windows/display.c
+++ src/ext/window/platform/windows/display.c
@@ -6,4 +6,22 @@
 
 __WINDOW_API_CALL void window_api_display_enumerate(sll_array_t* out){
 	SLL_INIT_ARRAY(out);
+	DWORD id=0;
+	while (1){
+		DISPLAY_DEVICEA device={
+			sizeof(DISPLAY_DEVICEA)
+		};
+		if (!EnumDisplayDevicesA(NULL,id,&device,0)){
+			break;
+		}
+		DEVMODEA settings={
+			sizeof(DEVMODEA)
+		};
+		if (!EnumDisplaySettingsA(device.DeviceName,ENUM_CURRENT_SETTINGS,&settings)){
+			goto _next_display;
+		}
+		sll_array_push(NULL,SLL_ACQUIRE_STATIC_INT(0),out);
+_next_display:
+		id++;
+	}
 }
