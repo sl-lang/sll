27fc423ebb3d9532e58ef1b4cc8764b787d9cea0
Krzem <krzem5.dev@gmail.com>
2022-07-03 10:45:33 +0200

    Push Update 03/07/2022, 10:45:33

--- src/ext/window/platform/linux/window.c
+++ src/ext/window/platform/linux/window.c
@@ -6,11 +6,14 @@
 
 __WINDOW_API_CALL void* window_api_window_create(int32_t x,int32_t y,uint32_t w,uint32_t h,void* parent){
 	xcb_window_t id=xcb_generate_id(_xcb_conn);
-	return (void*)id;
+	xcb_create_window(_xcb_conn,XCB_COPY_FROM_PARENT,id,(parent!=(void*)(0xffffffffffffffffull)?_xcb_screen->root:(int)(intptr_t)parent),x,y,w,h,10,XCB_WINDOW_CLASS_INPUT_OUTPUT,_xcb_screen->root_visual,0,NULL);
+	xcb_flush(_xcb_conn);
+	return (void*)(intptr_t)id;
 }
 
 
 
 __WINDOW_API_CALL void window_api_window_update_visibility(void* id,sll_bool_t show){
-	return;
+	(show?xcb_map_window:xcb_unmap_window)(_xcb_conn,(int)(intptr_t)id);
+	xcb_flush(_xcb_conn);
 }
