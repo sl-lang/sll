880709f60982fede5388571b3fb679364c722402
Krzem <krzem5.dev@gmail.com>
2022-07-03 10:51:38 +0200

    Push Update 03/07/2022, 10:51:38

--- src/sll/include/sll/_size_types.h
+++ src/sll/include/sll/_size_types.h
@@ -21,21 +21,21 @@
 
 
 
-#define __SLL_U1_MAX 1
-#define __SLL_U8_MAX 255
-#define __SLL_U16_MAX 65535
-#define __SLL_U32_MAX 4294967295u
-#define __SLL_U64_MAX 18446744073709551615ull
+#define __SLL_U1_MAX (1)
+#define __SLL_U8_MAX (255)
+#define __SLL_U16_MAX (65535)
+#define __SLL_U32_MAX (4294967295u)
+#define __SLL_U64_MAX (18446744073709551615ull)
 
-#define __SLL_S8_MIN -128
-#define __SLL_S16_MIN -32768
-#define __SLL_S32_MIN -2147483648
-#define __SLL_S64_MIN -9223372036854775808ll
+#define __SLL_S8_MIN (-128)
+#define __SLL_S16_MIN (-32768)
+#define __SLL_S32_MIN (-2147483647-1)
+#define __SLL_S64_MIN (-9223372036854775808ll)
 
-#define __SLL_S8_MAX 127
-#define __SLL_S16_MAX 32767
-#define __SLL_S32_MAX 2147483647
-#define __SLL_S64_MAX 9223372036854775807ll
+#define __SLL_S8_MAX (127)
+#define __SLL_S16_MAX (32767)
+#define __SLL_S32_MAX (2147483647)
+#define __SLL_S64_MAX (9223372036854775807ll)
 
 
 
