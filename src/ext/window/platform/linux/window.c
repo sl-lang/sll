eb8c5e2037452372254995a1155c1b67c2580fc4
Krzem <krzem5.dev@gmail.com>
2022-07-04 21:05:48 +0200

    Push Update 04/07/2022, 21:05:48

--- src/ext/window/platform/linux/window.c
+++ src/ext/window/platform/linux/window.c
@@ -1,12 +1,14 @@
 #include <sll.h>
 #include <stdint.h>
+#include <stdlib.h>
 #include <window/common.h>
 
 
 
 __WINDOW_API_CALL void* window_api_window_create(int32_t x,int32_t y,uint32_t w,uint32_t h,void* parent){
 	xcb_window_t id=xcb_generate_id(_xcb_conn);
-	xcb_create_window(_xcb_conn,XCB_COPY_FROM_PARENT,id,(parent==(void*)(0xffffffffffffffffull)?_xcb_screen->root:(int)(intptr_t)parent),x,y,w,h,10,XCB_WINDOW_CLASS_INPUT_OUTPUT,_xcb_screen->root_visual,0,NULL);
+	const uint32_t data[1]={XCB_EVENT_MASK_KEY_PRESS|XCB_EVENT_MASK_KEY_RELEASE|XCB_EVENT_MASK_BUTTON_PRESS|XCB_EVENT_MASK_BUTTON_RELEASE|XCB_EVENT_MASK_ENTER_WINDOW|XCB_EVENT_MASK_LEAVE_WINDOW|XCB_EVENT_MASK_POINTER_MOTION|XCB_EVENT_MASK_EXPOSURE|XCB_EVENT_MASK_VISIBILITY_CHANGE|XCB_EVENT_MASK_STRUCTURE_NOTIFY|XCB_EVENT_MASK_FOCUS_CHANGE|XCB_EVENT_MASK_PROPERTY_CHANGE};
+	xcb_create_window(_xcb_conn,XCB_COPY_FROM_PARENT,id,(parent==(void*)(0xffffffffffffffffull)?_xcb_screen->root:(int)(intptr_t)parent),x,y,w,h,10,XCB_WINDOW_CLASS_INPUT_OUTPUT,_xcb_screen->root_visual,XCB_CW_EVENT_MASK,data);
 	xcb_flush(_xcb_conn);
 	return (void*)(intptr_t)id;
 }
@@ -21,6 +23,13 @@ __WINDOW_API_CALL void window_api_window_destroy(void* id){
 
 
 __WINDOW_API_CALL void window_api_window_poll_events(void){
+	while (1){
+		xcb_generic_event_t* event=xcb_poll_for_event(_xcb_conn);
+		if (!event){
+			return;
+		}
+		free(event);
+	}
 }
 
 
