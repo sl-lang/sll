0d556076d442fb2a09ee9b9be7383540367a132d
Krzem <krzem5.dev@gmail.com>
2022-07-09 13:34:37 +0200

    Push Update 09/07/2022, 13:34:37

--- src/sll/map_container.c
+++ src/sll/map_container.c
@@ -29,7 +29,8 @@ static sll_size_t _lookup_entry(const sll_map_container_t* c,sll_size_t hash,con
 	sll_size_t i=hash&c->_mask;
 	for (sll_size_t j=0;j<c->_elem_count;j++){
 		sll_size_t k=*(c->_indices+i);
-		if (k!=0xffffffffffffffffull&&(c->data+k)->_hash==hash){
+		SLL_ASSERT(k==0xffffffffffffffffull||k<c->_elem_count);
+		if (k!=0xffffffffffffffffull&&!(c->data+k)->_delete&&(c->data+k)->_hash==hash){
 			const void* entry_key=(c->data+k)->key;
 			if ((c->_hash.is_function&&c->_eq_fn(entry_key,key))||(!c->_hash.is_function&&c->_hash.data.size&&sll_compare_data(entry_key,key,c->_hash.data.size))||(!c->_hash.is_function&&!c->_hash.data.size&&entry_key==key)){
 				return i;
@@ -44,7 +45,7 @@ static sll_size_t _lookup_entry(const sll_map_container_t* c,sll_size_t hash,con
 
 static void _index_element(const sll_map_container_t* c,sll_size_t i){
 	sll_size_t j=(c->data+i)->_hash&c->_mask;
-	while (*(c->_indices+j)!=0xffffffffffffffffull){
+	while (c->_indices[j]!=0xffffffffffffffffull){
 		j=(j+1)&c->_mask;
 	}
 	c->_indices[j]=i;
@@ -84,6 +85,8 @@ __SLL_EXTERNAL __SLL_CHECK_OUTPUT void* sll_map_container_delete(sll_map_contain
 		return default_;
 	}
 	sll_size_t data_index=*(c->_indices+index);
+	SLL_ASSERT(data_index<c->_elem_count);
+	SLL_ASSERT(index<=c->_mask);
 	*(c->_indices+index)=0xffffffffffffffffull;
 	(c->data+data_index)->_delete=1;
 	return (c->data+data_index)->value;
@@ -99,10 +102,12 @@ __SLL_EXTERNAL sll_bool_t sll_map_container_filter(sll_map_container_t* c,sll_ma
 			continue;
 		}
 		out=1;
+		e->_delete=1;
 		sll_size_t j=0;
 		while (*(c->_indices+j)!=i){
 			j++;
 		}
+		SLL_ASSERT(j<=c->_mask);
 		*(c->_indices+j)=0;
 	}
 	return out;
@@ -143,6 +148,9 @@ __SLL_EXTERNAL void sll_map_container_init(sll_map_container_hash_callback_t has
 
 __SLL_EXTERNAL void sll_map_container_iter(sll_map_container_t* c,sll_container_callback_t callback){
 	for (sll_size_t i=0;i<c->_elem_count;i++){
+		if ((c->data+i)->_delete){
+			continue;
+		}
 		callback((c->data+i)->value);
 	}
 }
@@ -160,6 +168,8 @@ __SLL_EXTERNAL void sll_map_container_set(sll_map_container_t* c,const void* key
 	sll_size_t hash=_hash_key(c,key);
 	sll_size_t index=_lookup_entry(c,hash,key);
 	if (index!=0xffffffffffffffffull){
+		SLL_ASSERT(index<=c->_mask);
+		SLL_ASSERT(*(c->_indices+index)<c->_elem_count);
 		(c->data+(*(c->_indices+index)))->value=value;
 		return;
 	}
