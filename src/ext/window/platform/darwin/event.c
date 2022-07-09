003e259581f99ad488b262e3e4321e23b0d9aa16
Krzem <krzem5.dev@gmail.com>
2022-07-09 15:47:04 +0200

    Push Update 09/07/2022, 15:47:04

--- src/ext/window/platform/darwin/event.c
+++ src/ext/window/platform/darwin/event.c
@@ -3,6 +3,6 @@
 
 
 
-__WINDOW_API_CALL void window_api_window_poll_events(sll_bool_t blocking,sll_array_t* out){
+__WINDOW_API_CALL void window_api_event_poll(sll_bool_t blocking,sll_array_t* out){
 	SLL_INIT_ARRAY(out);
 }
