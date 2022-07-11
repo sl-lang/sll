c92f904e5f9919bae69fac3ccfbbc1b72a7031cd
Krzem <krzem5.dev@gmail.com>
2022-07-11 21:28:01 +0200

    Push Update 11/07/2022, 21:28:01

--- src/sll/api_call.c
+++ src/sll/api_call.c
@@ -18,19 +18,19 @@
 
 sll_object_t* _call_api_func(sll_function_index_t fn,sll_object_t*const* al,sll_arg_count_t all){
 	const sll_internal_function_t* dt=sll_current_runtime_data->internal_function_table->data+fn;
-	void* bf=sll_allocate_stack(dt->_arg_size);
+	void* buffer=sll_allocate_stack(dt->_arg_size);
 	arg_output_t arg_output={
 		ARG_OUTPUT_TYPE_ARRAY,
 		{
 			.array={
-				bf,
+				buffer,
 				dt->_arg_size
 			}
 		}
 	};
-	sll_arg_state_t st=_parse_args_raw(dt->format,al,all,&arg_output);
+	sll_arg_state_t arg_state=_parse_args_raw(dt->format,al,all,&arg_output);
 	api_return_value_t ret;
-	sll_integer_t ret_i=_call_api_func_assembly(&ret,dt->_registers,bf,dt->_arg_count,dt->function);
+	sll_integer_t ret_i=_call_api_func_assembly(&ret,dt->_registers,buffer,dt->_arg_count,dt->function);
 	sll_object_t* out;
 	if ((dt->_return_value&RETURN_VALUE_FLAG_ERROR)&&ret_i){
 		if (RETURN_VALUE_GET_TYPE(dt->_return_value)=='b'||RETURN_VALUE_GET_TYPE(dt->_return_value)=='B'||RETURN_VALUE_GET_TYPE(dt->_return_value)=='W'||RETURN_VALUE_GET_TYPE(dt->_return_value)=='D'||RETURN_VALUE_GET_TYPE(dt->_return_value)=='Q'||RETURN_VALUE_GET_TYPE(dt->_return_value)=='i'){
@@ -98,8 +98,8 @@ sll_object_t* _call_api_func(sll_function_index_t fn,sll_object_t*const* al,sll_
 				SLL_UNREACHABLE();
 		}
 	}
-	sll_free_args(st);
-	sll_deallocate(bf);
+	sll_free_args(arg_state);
+	sll_deallocate(buffer);
 	return out;
 }
 
