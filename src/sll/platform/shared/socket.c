8207a7a74e54b8f4ea8e285dcf34aa408b85349f
Krzem <krzem5.dev@gmail.com>
2022-07-13 21:36:56 +0200

    Push Update 13/07/2022, 21:36:56

---
+++ src/sll/platform/shared/socket.c
@@ -0,0 +1,13 @@
+#include <sll/_internal/common.h>
+#include <sll/common.h>
+#include <sll/error.h>
+#include <sll/platform/socket.h>
+#include <sll/socket.h>
+#include <sll/types.h>
+
+
+
+
+__SLL_EXTERNAL __SLL_CHECK_OUTPUT sll_error_t sll_platform_socket_get_address_info(const sll_char_t* node,const sll_char_t* service,sll_socket_ddress_family_t address_family,sll_socket_type_t type,sll_socket_protocol_t protocol,sll_address_info_flags_t flags,sll_address_info_t** out,sll_address_info_count_t* out_count){
+	SLL_UNIMPLEMENTED();
+}
