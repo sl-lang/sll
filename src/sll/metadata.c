f70f1117ee103282a30b9b6bf30a740967eb738e
Krzem <krzem5.dev@gmail.com>
2022-07-26 10:25:33 +0200

    Push Update 26/07/2022, 10:25:33

--- src/sll/metadata.c
+++ src/sll/metadata.c
@@ -99,16 +99,15 @@ static sll_node_t* _update(sll_node_t* o,sll_string_index_t* sm){
 		}
 	}
 	switch (o->type){
+		case SLL_NODE_TYPE_STRING:
+		case SLL_NODE_TYPE_FIELD:
+			o->data.string_index=*(sm+o->data.string_index);
 		case SLL_NODE_TYPE_INT:
 		case SLL_NODE_TYPE_FLOAT:
 		case SLL_NODE_TYPE_CHAR:
 		case SLL_NODE_TYPE_COMPLEX:
 		case SLL_NODE_TYPE_IDENTIFIER:
 		case SLL_NODE_TYPE_FUNCTION_ID:
-			return o+1;
-		case SLL_NODE_TYPE_STRING:
-		case SLL_NODE_TYPE_FIELD:
-			o->data.string_index=*(sm+o->data.string_index);
 			return o+1;
 		case SLL_NODE_TYPE_FUNC:
 		case SLL_NODE_TYPE_INTERNAL_FUNC:
