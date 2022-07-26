dccf9a949818b3a76170e68353f499e749e6b593
Krzem <krzem5.dev@gmail.com>
2022-07-26 09:15:06 +0200

    Push Update 26/07/2022, 09:15:06

--- src/sll/size.c
+++ src/sll/size.c
@@ -15,68 +15,13 @@ static const sll_node_t* _get_node_size(const sll_node_t* node,sll_node_offset_t
 		}
 	}
 	(*size)++;
-	switch (node->type){
-		case SLL_NODE_TYPE_INT:
-		case SLL_NODE_TYPE_FLOAT:
-		case SLL_NODE_TYPE_CHAR:
-		case SLL_NODE_TYPE_COMPLEX:
-		case SLL_NODE_TYPE_STRING:
-		case SLL_NODE_TYPE_IDENTIFIER:
-		case SLL_NODE_TYPE_FIELD:
-		case SLL_NODE_TYPE_FUNCTION_ID:
-			return node+1;
-		case SLL_NODE_TYPE_ARRAY:
-			{
-				sll_array_length_t l=node->data.array_length;
-				node++;
-				while (l){
-					l--;
-					node=_get_node_size(node,size);
-				}
-				return node;
-			}
-		case SLL_NODE_TYPE_MAP:
-			{
-				sll_map_length_t l=node->data.map_length;
-				node++;
-				while (l){
-					l--;
-					node=_get_node_size(node,size);
-				}
-				return node;
-			}
-		case SLL_NODE_TYPE_FUNC:
-		case SLL_NODE_TYPE_INTERNAL_FUNC:
-			{
-				sll_arg_count_t l=node->data.function.arg_count;
-				node++;
-				while (l){
-					l--;
-					node=_get_node_size(node,size);
-				}
-				return node;
-			}
-		case SLL_NODE_TYPE_FOR:
-		case SLL_NODE_TYPE_WHILE:
-		case SLL_NODE_TYPE_LOOP:
-		case SLL_NODE_TYPE_FOR_ARRAY:
-		case SLL_NODE_TYPE_WHILE_ARRAY:
-		case SLL_NODE_TYPE_FOR_MAP:
-		case SLL_NODE_TYPE_WHILE_MAP:
-			{
-				sll_arg_count_t l=node->data.loop.arg_count;
-				node++;
-				while (l){
-					l--;
-					node=_get_node_size(node,size);
-				}
-				return node;
-			}
+	if (!SLL_NODE_HAS_CHILDREN(node)){
+		return node+1;
 	}
-	sll_arg_count_t l=node->data.arg_count;
+	sll_arg_count_t arg_count=node->data.arg_count;
 	node++;
-	while (l){
-		l--;
+	while (arg_count){
+		arg_count--;
 		node=_get_node_size(node,size);
 	}
 	return node;
--- src/sll/skip.c
+++ src/sll/skip.c
@@ -8,68 +8,13 @@ __SLL_EXTERNAL __SLL_CHECK_OUTPUT sll_node_t* sll_skip_node(sll_node_t* node){
 	while (node->type==SLL_NODE_TYPE_NOP||node->type==SLL_NODE_TYPE_DBG||node->type==SLL_NODE_TYPE_CHANGE_STACK){
 		node=(node->type==SLL_NODE_TYPE_CHANGE_STACK?node->data._next_node:node+1);
 	}
-	switch (node->type){
-		case SLL_NODE_TYPE_INT:
-		case SLL_NODE_TYPE_FLOAT:
-		case SLL_NODE_TYPE_CHAR:
-		case SLL_NODE_TYPE_COMPLEX:
-		case SLL_NODE_TYPE_STRING:
-		case SLL_NODE_TYPE_IDENTIFIER:
-		case SLL_NODE_TYPE_FIELD:
-		case SLL_NODE_TYPE_FUNCTION_ID:
-			return node+1;
-		case SLL_NODE_TYPE_ARRAY:
-			{
-				sll_array_length_t l=node->data.array_length;
-				node++;
-				while (l){
-					l--;
-					node=sll_skip_node(node);
-				}
-				return node;
-			}
-		case SLL_NODE_TYPE_MAP:
-			{
-				sll_map_length_t l=node->data.map_length;
-				node++;
-				while (l){
-					l--;
-					node=sll_skip_node(node);
-				}
-				return node;
-			}
-		case SLL_NODE_TYPE_FUNC:
-		case SLL_NODE_TYPE_INTERNAL_FUNC:
-			{
-				sll_arg_count_t l=node->data.function.arg_count;
-				node++;
-				while (l){
-					l--;
-					node=sll_skip_node(node);
-				}
-				return node;
-			}
-		case SLL_NODE_TYPE_FOR:
-		case SLL_NODE_TYPE_WHILE:
-		case SLL_NODE_TYPE_LOOP:
-		case SLL_NODE_TYPE_FOR_ARRAY:
-		case SLL_NODE_TYPE_WHILE_ARRAY:
-		case SLL_NODE_TYPE_FOR_MAP:
-		case SLL_NODE_TYPE_WHILE_MAP:
-			{
-				sll_arg_count_t l=node->data.loop.arg_count;
-				node++;
-				while (l){
-					l--;
-					node=sll_skip_node(node);
-				}
-				return node;
-			}
+	if (!SLL_NODE_HAS_CHILDREN(node)){
+		return node+1;
 	}
-	sll_arg_count_t l=node->data.arg_count;
+	sll_arg_count_t arg_count=node->data.arg_count;
 	node++;
-	while (l){
-		l--;
+	while (arg_count){
+		arg_count--;
 		node=sll_skip_node(node);
 	}
 	return node;
