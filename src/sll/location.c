a90887df753a461306496760cd935b20729a2776
Krzesimir Hyżyk <krzem5.dev@gmail.com>
2022-07-29 12:51:41 +0100

    Push Update 29/07/2022, 12:51:41

--- src/sll/location.c
+++ src/sll/location.c
@@ -12,42 +12,42 @@
 
 
 __SLL_EXTERNAL sll_file_offset_t sll_get_location(const sll_assembly_data_t* assembly_data,sll_instruction_index_t instruction_index,sll_string_index_t* file_path_string_index,sll_string_index_t* function_string_index){
-	sll_string_index_t o_fp=(assembly_data->string_table.length?0:SLL_MAX_STRING_INDEX);
+	sll_string_index_t file_path=(assembly_data->string_table.length?0:SLL_MAX_STRING_INDEX);
 	if (instruction_index>=assembly_data->instruction_count){
 		if (file_path_string_index){
-			*file_path_string_index=o_fp;
+			*file_path_string_index=file_path;
 		}
 		if (function_string_index){
 			*function_string_index=SLL_MAX_STRING_INDEX;
 		}
 		return 0;
 	}
-	sll_instruction_index_t c=0;
-	sll_file_offset_t o_ln=0;
-	sll_string_index_t o_fn=SLL_MAX_STRING_INDEX;
+	sll_instruction_index_t counter=0;
+	sll_file_offset_t line=0;
+	sll_string_index_t function=SLL_MAX_STRING_INDEX;
 	for (sll_debug_data_length_t i=0;i<assembly_data->debug_data.length;i++){
-		c+=(assembly_data->debug_data.data+i)->delta_instruction_index;
-		if (c>instruction_index){
+		counter+=(assembly_data->debug_data.data+i)->delta_instruction_index;
+		if (counter>instruction_index){
 			break;
 		}
 		if ((assembly_data->debug_data.data+i)->line&SLL_DEBUG_LINE_DATA_FLAG_FILE){
-			o_fp=(sll_string_index_t)SLL_DEBUG_LINE_DATA_GET_DATA(assembly_data->debug_data.data+i);
-			o_ln=0;
+			file_path=(sll_string_index_t)SLL_DEBUG_LINE_DATA_GET_DATA(assembly_data->debug_data.data+i);
+			line=0;
 		}
 		else if ((assembly_data->debug_data.data+i)->line&SLL_DEBUG_LINE_DATA_FLAG_FUNC){
-			o_fn=(sll_string_index_t)SLL_DEBUG_LINE_DATA_GET_DATA(assembly_data->debug_data.data+i);
+			function=(sll_string_index_t)SLL_DEBUG_LINE_DATA_GET_DATA(assembly_data->debug_data.data+i);
 		}
 		else{
-			o_ln=SLL_DEBUG_LINE_DATA_GET_DATA(assembly_data->debug_data.data+i);
+			line=SLL_DEBUG_LINE_DATA_GET_DATA(assembly_data->debug_data.data+i);
 		}
 	}
 	if (file_path_string_index){
-		*file_path_string_index=o_fp;
+		*file_path_string_index=file_path;
 	}
 	if (function_string_index){
-		*function_string_index=o_fn;
+		*function_string_index=function;
 	}
-	return o_ln+1;
+	return line+1;
 }
 
 
