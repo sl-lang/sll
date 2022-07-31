ff9e720e6fef64109215c9fc03085cac5973712d
Krzesimir Hyżyk <krzem5.dev@gmail.com>
2022-07-31 13:15:48 +0100

    Push Update 31/07/2022, 13:15:48

--- src/sll/size.c
+++ src/sll/size.c
@@ -9,19 +9,13 @@ __SLL_EXTERNAL __SLL_CHECK_OUTPUT sll_node_offset_t sll_get_node_size(sll_node_t
 	sll_node_offset_t stack=1;
 	while (stack){
 		stack--;
-		while (node->type==SLL_NODE_TYPE_NOP||node->type==SLL_NODE_TYPE_DBG||node->type==SLL_NODE_TYPE_CHANGE_STACK){
-			if (node->type==SLL_NODE_TYPE_CHANGE_STACK){
-				node=node->data._next_node;
-			}
-			else{
-				out++;
-				node++;
-			}
+		if (node->type==SLL_NODE_TYPE_CHANGE_STACK){
+			node=node->data._next_node;
 		}
-		out++;
 		if (SLL_NODE_HAS_CHILDREN(node)){
 			stack+=node->data.arg_count;
 		}
+		out++;
 		node++;
 	}
 	return out;
