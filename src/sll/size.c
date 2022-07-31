bf3e5c581808d2709dc15e6b48884d2f903bb0ff
Krzesimir Hyżyk <krzem5.dev@gmail.com>
2022-07-31 13:14:25 +0100

    Push Update 31/07/2022, 13:14:25

--- src/sll/size.c
+++ src/sll/size.c
@@ -4,33 +4,25 @@
 
 
 
-static sll_node_t _get_node_size(sll_node_t node,sll_node_offset_t* size){
-	while (node->type==SLL_NODE_TYPE_NOP||node->type==SLL_NODE_TYPE_DBG||node->type==SLL_NODE_TYPE_CHANGE_STACK){
-		if (node->type==SLL_NODE_TYPE_CHANGE_STACK){
-			node=node->data._next_node;
-		}
-		else{
-			(*size)++;
-			node++;
-		}
-	}
-	(*size)++;
-	if (!SLL_NODE_HAS_CHILDREN(node)){
-		return node+1;
-	}
-	sll_arg_count_t arg_count=node->data.arg_count;
-	node++;
-	while (arg_count){
-		arg_count--;
-		node=_get_node_size(node,size);
-	}
-	return node;
-}
-
-
-
 __SLL_EXTERNAL __SLL_CHECK_OUTPUT sll_node_offset_t sll_get_node_size(sll_node_t node){
 	sll_node_offset_t out=0;
-	_get_node_size(node,&out);
+	sll_node_offset_t stack=1;
+	while (stack){
+		stack--;
+		while (node->type==SLL_NODE_TYPE_NOP||node->type==SLL_NODE_TYPE_DBG||node->type==SLL_NODE_TYPE_CHANGE_STACK){
+			if (node->type==SLL_NODE_TYPE_CHANGE_STACK){
+				node=node->data._next_node;
+			}
+			else{
+				out++;
+				node++;
+			}
+		}
+		out++;
+		if (SLL_NODE_HAS_CHILDREN(node)){
+			stack+=node->data.arg_count;
+		}
+		node++;
+	}
 	return out;
 }
