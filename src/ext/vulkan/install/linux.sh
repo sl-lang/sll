9f02a7f847e289587e49f762207cae91bfc7246a
Krzesimir Hyżyk <krzem5.dev@gmail.com>
2022-08-17 22:21:08 +0200

    Push Update 17/08/2022, 22:21:08

--- src/ext/vulkan/install/linux.sh
+++ src/ext/vulkan/install/linux.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt install vulkan-tools
-if [ ! -d "build/ext/vulkan/headers" ]; then
-	mkdir build/ext/vulkan/headers
-	git clone https://github.com/KhronosGroup/Vulkan-Headers.git build/ext/vulkan/headers
+if [ ! -d "build/ext/vulkan_linux/headers" ]; then
+	mkdir build/ext/vulkan_linux/headers
+	git clone https://github.com/KhronosGroup/Vulkan-Headers.git build/ext/vulkan_linux/headers
 fi
---
+++ src/ext/vulkan/install/windows.bat
@@ -0,0 +1,2 @@
+curl -L -o build/ext/vulkan_windows/installer.exe "https://sdk.lunarg.com/sdk/download/latest/windows/vulkan-sdk.exe"
+build/ext/vulkan_windows/installer.exe /S
