b0334290bec1ff500d3cca7bf3d8934bfdd32853
Krzesimir Hyżyk <krzem5.dev@gmail.com>
2022-08-18 09:16:49 +0200

    Push Update 18/08/2022, 09:16:49

--- src/ext/vulkan/install/windows.bat
+++ src/ext/vulkan/install/windows.bat
@@ -1,7 +1,12 @@
 @echo off
+echo $$$ Line 1
 curl -L -o build/ext/vulkan_windows/installer.exe "https://sdk.lunarg.com/sdk/download/latest/windows/vulkan-sdk.exe"
+echo $$$ Line 2
 build/ext/vulkan_windows/installer.exe /S
+echo $$$ Line 3
 if not exist build\ext\vulkan_windows\headers (
+	echo $$$ Line 4
 	mkdir build\ext\vulkan_windows\headers
+	echo $$$ Line 5
 	git clone "https://github.com/KhronosGroup/Vulkan-Headers.git" build\ext\vulkan_windows\headers
 )
