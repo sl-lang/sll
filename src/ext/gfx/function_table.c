4463916dbf507fcf300126b9bb8512a67dc193f9
Krzesimir Hyżyk <krzem5.dev@gmail.com>
2022-08-29 14:21:25 +0200

    Push Update 29/08/2022, 14:21:25

--- src/ext/gfx/function_table.c
+++ src/ext/gfx/function_table.c
@@ -68,7 +68,7 @@ static const sll_internal_function_descriptor_t _internal_function_table[]={
 	{
 		SLL_CHAR("gfx:pipeline_create"),
 		gfx_api_pipeline_create,
-		SLL_CHAR("QB#aD#aBbB#a|Q")
+		SLL_CHAR("QB#aD#a#aBbB#a|Q")
 	},
 	{
 		SLL_CHAR("gfx:pipeline_delete"),
--- src/ext/gfx/include/gfx/pipeline.h
+++ src/ext/gfx/include/gfx/pipeline.h
@@ -65,7 +65,7 @@ void _delete_pipeline(const gfx_context_data_t* ctx,gfx_pipeline_data_t* pipelin
 
 
 
-__GFX_API_CALL gfx_pipeline_t gfx_api_pipeline_create(gfx_context_t ctx_id,gfx_pipeline_topology_t topology,const sll_array_t* input_attributes,uint32_t stride,const sll_array_t* uniform_buffers,gfx_pipeline_polygone_mode_t polygon_mode,gfx_pipeline_front_face_t front_face,gfx_pipeline_cull_mode_t cull_mode,const sll_array_t* shaders);
+__GFX_API_CALL gfx_pipeline_t gfx_api_pipeline_create(gfx_context_t ctx_id,gfx_pipeline_topology_t topology,const sll_array_t* input_attributes,uint32_t stride,const sll_array_t* uniform_buffers,const sll_array_t* samplers,gfx_pipeline_polygone_mode_t polygon_mode,gfx_pipeline_front_face_t front_face,gfx_pipeline_cull_mode_t cull_mode,const sll_array_t* shaders);
 
 
 
