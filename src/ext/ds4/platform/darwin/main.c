1d897fb8012e024982536048bbfe5dcc33bf51bf
Krzesimir Hyżyk <krzem5.dev@gmail.com>
2022-09-12 22:03:38 +0200

    Push Update 12/09/2022, 22:03:38

---
+++ src/ext/ds4/platform/darwin/main.c
@@ -0,0 +1,14 @@
+#include <ds4/common.h>
+#include <sll.h>
+
+
+
+__DS4_API_CALL void ds4_api_device_list(sll_array_t* out){
+	SLL_INIT_ARRAY(out);
+}
+
+
+
+__DS4_EXTERNAL sll_bool_t SLL_ABI_INIT(sll_version_t version){
+	return (version==SLL_VERSION);
+}
