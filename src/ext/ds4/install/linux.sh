525db7a40bec4aa1eceff934e62f7518b21c95d7
Krzesimir Hyżyk <krzem5.dev@gmail.com>
2022-09-13 17:28:27 +0200

    Push Update 13/09/2022, 17:28:27

--- src/ext/ds4/install/linux.sh
+++ src/ext/ds4/install/linux.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-sudo apt install udev libudev-dev
+packages_to_install=""
+for pkg in "udev libudev-dev"; do
+	if [ "" = "$(dpkg-query -W --showformat='${Status}\n' $pkg|grep "install ok installed")" ]; then
+		packages_to_install+=" $pkg"
+	fi
+done
+if [ "" != "$packages_to_install" ]; then
+	eval "sudo apt install$packages_to_install"
+fi
