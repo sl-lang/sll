b8d69daad56c50ab708d69ec8861b662cd9fc62a
Krzesimir Hyżyk <krzem5.dev@gmail.com>
2022-09-13 18:02:33 +0200

    Push Update 13/09/2022, 18:02:33

--- src/ext/window_manager/install/linux.sh
+++ src/ext/window_manager/install/linux.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
+required_packages=( "libxcb-randr0" "libxcb-randr0-dev" "libxcb-xkb-dev" "libxcb-icccm4" "libxcb-icccm4-dev" "libxcb-cursor0" "libxcb-cursor-dev" )
 packages_to_install=""
-for pkg in "libxcb-randr0 libxcb-randr0-dev libxcb-xkb-dev libxcb-icccm4 libxcb-icccm4-dev libxcb-cursor0 libxcb-cursor-dev"; do
+for pkg in "${required_packages[@]}"; do
 	if [ "" = "$(dpkg-query -W --showformat='${Status}\n' $pkg|grep "install ok installed")" ]; then
 		packages_to_install+=" $pkg"
 	fi
 done
 if [ "" != "$packages_to_install" ]; then
-	echo "Installing packages:$packages_to_install"
 	eval "sudo apt install$packages_to_install"
 fi
