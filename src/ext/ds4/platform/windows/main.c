12c48cb803a0705187f95f57dff8c7835d3dee2d
Krzesimir Hyżyk <krzem5.dev@gmail.com>
2022-09-13 21:29:31 +0200

    Push Update 13/09/2022, 21:29:31

--- src/ext/ds4/platform/windows/main.c
+++ src/ext/ds4/platform/windows/main.c
@@ -6,6 +6,55 @@
 
 __DS4_API_CALL void ds4_api_device_list(sll_array_t* out){
 	SLL_INIT_ARRAY(out);
+	GUID guid=GUID_DEVINTERFACE_HID;
+	HDEVINFO dev_list=SetupDiGetClassDevsA(&guid,NULL,NULL,DIGCF_PRESENT|DIGCF_DEVICEINTERFACE);
+	uint32_t dev_list_idx=0;
+	while (1){
+		SP_DEVICE_INTERFACE_DATA dev_interface={
+			sizeof(SP_DEVICE_INTERFACE_DATA)
+		};
+		if (!SetupDiEnumDeviceInterfaces(dev_list,NULL,&guid,dev_list_idx,&dev_interface)){
+			break;
+		}
+		dev_list_idx++;
+		DWORD size;
+		if (!SetupDiGetDeviceInterfaceDetailA(dev_list,&dev_interface,NULL,0,&size,NULL)&&GetLastError()!=ERROR_INSUFFICIENT_BUFFER){
+			continue;
+		}
+		SP_DEVICE_INTERFACE_DETAIL_DATA_A dev={
+			sizeof(SP_DEVICE_INTERFACE_DETAIL_DATA_A)
+		};
+ 		if (!SetupDiGetDeviceInterfaceDetailA(dev_list,&dev_interface,&dev,size,NULL,NULL)){
+			continue;
+		}
+		DWORD j=0;
+		do{
+			SP_DEVINFO_DATA dev_info={
+				sizeof(SP_DEVINFO_DATA)
+			};
+			if (!SetupDiEnumDeviceInfo(dev_list,j,&dev_info)){
+				goto _check_next_device;
+			}
+			j++;
+			char dev_name[MAX_PATH];
+			if (!SetupDiGetDeviceRegistryPropertyA(dev_list,&dev_info,SPDRP_CLASS,NULL,(PBYTE)dev_name,MAX_PATH,NULL)||sll_string_compare_pointer(SLL_CHAR(dev_name),SLL_CHAR("HIDClass"))){
+				continue;
+			}
+		} while (!SetupDiGetDeviceRegistryPropertyA(dev_list,&dev_info,SPDRP_DRIVER,NULL,(PBYTE)dev_name,MAX_PATH,NULL));
+		HANDLE fh=CreateFileA(dev.DevicePath,0,FILE_SHARE_READ|FILE_SHARE_WRITE,NULL,OPEN_EXISTING,FILE_FLAG_OVERLAPPED,0);
+		if (fh==INVALID_HANDLE_VALUE){
+			continue;
+		}
+		HIDD_ATTRIBUTES attributes={
+			sizeof(HIDD_ATTRIBUTES)
+		};
+		if (HidD_GetAttributes(fh,&attributes)&&attributes.VendorID==SONY_VENDOR_ID&&(attributes.ProductID==DS4_PRODUT_ID1||attributes.ProductID==DS4_PRODUT_ID2)){
+			sll_array_push(NULL,sll_string_pointer_to_object(SLL_CHAR(dev.DevicePath)),out);
+		}
+		CloseHandle(fh);
+_check_next_device:;
+	}
+	SetupDiDestroyDeviceInfoList(dev_list);
 }
 
 
