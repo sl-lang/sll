76dc76ec1e4366789e9afe5e6431c12ccb8fe28c
Krzesimir Hyżyk <krzem5.dev@gmail.com>
2022-09-16 17:43:00 +0200

    Push Update 16/09/2022, 17:43:00

--- src/ext/font/install/linux.sh
+++ src/ext/font/install/linux.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
+required_packages=( "libfontconfig" "libfontconfig-dev" )
+packages_to_install=""
+for pkg in "${required_packages[@]}"; do
+	if [ "" = "$(dpkg-query -W --showformat='${Status}\n' $pkg|grep "install ok installed")" ]; then
+		packages_to_install+=" $pkg"
+	fi
+done
+if [ "" != "$packages_to_install" ]; then
+	eval "sudo apt install$packages_to_install"
+fi
 mkdir "build/ext/font_linux/stb_libraries"
 git clone https://github.com/nothings/stb.git "build/ext/font_linux/stb_libraries"
