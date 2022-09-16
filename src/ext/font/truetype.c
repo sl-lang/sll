784daeeae0f533cc38c6899241494edffb46dc44
Krzesimir Hyżyk <krzem5.dev@gmail.com>
2022-09-16 20:02:08 +0200

    Push Update 16/09/2022, 20:02:08

--- src/ext/font/truetype.c
+++ src/ext/font/truetype.c
@@ -7,6 +7,12 @@
 
 
 __FONT_API_CALL void font_api_true_type_create(const sll_string_t* data,sll_array_t* out){
+	stbtt_fontinfo font;
+	stbtt_InitFont(&font,data->data,0);
+	int ascent;
+	int descent;
+	int line_gap;
+	stbtt_GetFontVMetrics(&font,&ascent,&descent,&line_gap);
 	uint8_t texture_data[1048576];
 	stbtt_pack_context pack_ctx;
 	stbtt_PackBegin(&pack_ctx,texture_data,1024,1024,0,1,NULL);
@@ -14,11 +20,5 @@ __FONT_API_CALL void font_api_true_type_create(const sll_string_t* data,sll_arra
 	stbtt_packedchar render_data[95];
 	stbtt_PackFontRange(&pack_ctx,data->data,0,64.0f,32,95,render_data);
 	stbtt_PackEnd(&pack_ctx);
-	stbtt_fontinfo font;
-	stbtt_InitFont(&font,data->data,0);
-	int ascent;
-	int descent;
-	int line_gap;
-	stbtt_GetFontVMetrics(&font,&ascent,&descent,&line_gap);
 	sll_new_object_array(SLL_CHAR("uul{MMMMFFFFF}hhh"),out,1024,1024,texture_data,1048576,render_data,95ull,sizeof(stbtt_packedchar),SLL_OFFSETOF(stbtt_packedchar,x0),0xffffull,SLL_OFFSETOF(stbtt_packedchar,y0),0xffffull,SLL_OFFSETOF(stbtt_packedchar,x1),0xffffull,SLL_OFFSETOF(stbtt_packedchar,y1),0xffffull,SLL_OFFSETOF(stbtt_packedchar,xoff),SLL_OFFSETOF(stbtt_packedchar,yoff),SLL_OFFSETOF(stbtt_packedchar,xoff2),SLL_OFFSETOF(stbtt_packedchar,yoff2),SLL_OFFSETOF(stbtt_packedchar,xadvance),ascent,descent,line_gap);
 }
