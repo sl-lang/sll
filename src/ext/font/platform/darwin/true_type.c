092fad2c9f0163d95ac7991c82e5cdee521ee32a
Krzesimir Hyżyk <krzem5.dev@gmail.com>
2022-09-16 18:24:06 +0200

    Push Update 16/09/2022, 18:24:06

---
+++ src/ext/font/platform/darwin/true_type.c
@@ -0,0 +1,9 @@
+#include <font/common.h>
+#include <font/true_type.h>
+#include <sll.h>
+
+
+
+__FONT_API_CALL void font_api_true_type_find(const sll_string_t* name,sll_string_t* out){
+	SLL_INIT_STRING(out);
+}
