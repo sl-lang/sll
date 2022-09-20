3e2d3816fa8547dbb96e4b1f72abdfa0ead232a6
Krzesimir Hyżyk <krzem5.dev@gmail.com>
2022-09-20 21:15:52 +0200

    Push Update 20/09/2022, 21:15:52

--- src/ext/font/truetype.c
+++ src/ext/font/truetype.c
@@ -6,6 +6,10 @@
 
 
 
+#define FONT_SIZE 64.0f
+
+
+
 __FONT_API_CALL void font_api_true_type_create(const sll_string_t* data,sll_array_t* out){
 	sll_array_t glyphs=SLL_INIT_ARRAY_STRUCT;
 	stbtt_fontinfo font;
@@ -14,11 +18,11 @@ __FONT_API_CALL void font_api_true_type_create(const sll_string_t* data,sll_arra
 	int descent;
 	int line_gap;
 	stbtt_GetFontVMetrics(&font,&ascent,&descent,&line_gap);
-	float scale=stbtt_ScaleForPixelHeight(&font,64.0f);
+	float scale=stbtt_ScaleForPixelHeight(&font,FONT_SIZE);
 	for (int i=32;i<127;i++){
 		int glyph=stbtt_FindGlyphIndex(&font,i);
 		if (!glyph){
-			sll_array_push(NULL,sll_new_object(SLL_CHAR("c00000Z"),i),&glyphs);
+			sll_array_push(NULL,sll_new_object(SLL_CHAR("c00000Z1"),i),&glyphs);
 			continue;
 		}
 		int x0;
@@ -33,8 +37,8 @@ __FONT_API_CALL void font_api_true_type_create(const sll_string_t* data,sll_arra
 		sll_size_t size=((sll_size_t)width)*height;
 		uint8_t* bitmap=sll_allocate_stack(size);
 		stbtt_MakeGlyphBitmap(&font,bitmap,width,height,width,scale,scale,glyph);
-		sll_array_push(NULL,sll_new_object(SLL_CHAR("chhuuul"),i,x0,y0,width,height,(int)(advance*scale),bitmap,size),&glyphs);
+		sll_array_push(NULL,sll_new_object(SLL_CHAR("cffffflu"),i,x0/FONT_SIZE,y0/FONT_SIZE,width/FONT_SIZE,height/FONT_SIZE,advance*scale/FONT_SIZE,bitmap,size,(width?width:1)),&glyphs);
 		sll_deallocate(bitmap);
 	}
-	sll_new_object_array(SLL_CHAR("hhha"),out,(int)(ascent*scale),(int)(descent*scale),(int)(line_gap*scale),&glyphs);
+	sll_new_object_array(SLL_CHAR("fffa"),out,ascent*scale/FONT_SIZE,descent*scale/FONT_SIZE,line_gap*scale/FONT_SIZE,&glyphs);
 }
