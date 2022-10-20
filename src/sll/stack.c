4f07072cab5f450386fbad3f4d549e2dd30b234d
Krzesimir Hyżyk <krzem5.dev@gmail.com>
2022-10-20 20:48:10 +0200

    Push Update 20/10/2022, 20:48:10

--- src/sll/stack.c
+++ src/sll/stack.c
@@ -121,14 +121,14 @@ void _init_assembly_stack(sll_assembly_data_t* assembly_data){
 	assembly_data->_instruction_stack.start=sll_platform_allocate_page(SLL_ROUND_PAGE(ASSEMBLY_INSTRUCTION_STACK_ALLOC_SIZE),0,NULL);
 	assembly_data->_instruction_stack.end=assembly_data->_instruction_stack.start;
 	((assembly_stack_page_t*)(assembly_data->_instruction_stack.start))->next=NULL;
-	sll_assembly_instruction_t s=PTR(ADDR(assembly_data->_instruction_stack.start)+sizeof(assembly_stack_page_t));
-	s->type=SLL_ASSEMBLY_INSTRUCTION_TYPE_CHANGE_STACK;
-	s->data._next_instruction=NULL;
+	sll_assembly_instruction_t change_stack_instruction=PTR(ADDR(assembly_data->_instruction_stack.start)+sizeof(assembly_stack_page_t));
+	change_stack_instruction->type=SLL_ASSEMBLY_INSTRUCTION_TYPE_CHANGE_STACK;
+	change_stack_instruction->data._next_instruction=NULL;
 	assembly_data->_instruction_stack.count=((SLL_ROUND_PAGE(ASSEMBLY_INSTRUCTION_STACK_ALLOC_SIZE)-sizeof(assembly_stack_page_t)-sizeof(struct _SLL_ASSEMBLY_INSTRUCTION)*2)/sizeof(struct _SLL_ASSEMBLY_INSTRUCTION));
-	assembly_data->_instruction_stack.next_instruction=s+1;
-	s+=assembly_data->_instruction_stack.count+1;
-	s->type=SLL_ASSEMBLY_INSTRUCTION_TYPE_CHANGE_STACK;
-	s->data._next_instruction=NULL;
+	assembly_data->_instruction_stack.next_instruction=change_stack_instruction+1;
+	change_stack_instruction+=assembly_data->_instruction_stack.count+1;
+	change_stack_instruction->type=SLL_ASSEMBLY_INSTRUCTION_TYPE_CHANGE_STACK;
+	change_stack_instruction->data._next_instruction=NULL;
 }
 
 
