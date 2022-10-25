0aadb5bc13064c9f7ab0e5ae1a5d38cde64d16d0
Krzesimir Hyżyk <krzem5.dev@gmail.com>
2022-10-25 10:44:21 +0200

    Push Update 25/10/2022, 10:44:21

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+apt-get update
+export DEBIAN_FRONTEND=noninteractive
+apt-get -y install --no-install-recommends nasm gcc
+apt-get clean
+rm -rf /var/lib/apt/lists/*
+mkdir /commandhistory
+touch /commandhistory/.bash_history
+chown -R vscode /commandhistory
+echo "export PROMPT_COMMAND='history -a' && export HISTFILE=/commandhistory/.bash_history" >> "/home/vscode/.bashrc"
