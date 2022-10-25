666e53343693a0bcd74f48443df5f15ac69a9d66
Krzesimir Hyżyk <krzem5.dev@gmail.com>
2022-10-25 10:58:11 +0200

    Push Update 25/10/2022, 10:58:11

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -7,4 +7,4 @@ rm -rf /var/lib/apt/lists/*
 mkdir /commandhistory
 touch /commandhistory/.bash_history
 chown -R vscode /commandhistory
-echo "export PROMPT_COMMAND='history -a' && export HISTFILE=/commandhistory/.bash_history" >> "/home/vscode/.bashrc"
+echo "export PROMPT_COMMAND='history -a'&&export HISTFILE=/commandhistory/.bash_history" >> "/home/vscode/.bashrc"
